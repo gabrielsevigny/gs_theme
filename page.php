039ac0e333c6b8f33df1dcb2b627cece3d338a46
gabrielsevigny <gabrielsevigny@me.com>
2020-08-22 01:17:54 -0400

    Update page.php

--- page.php
+++ page.php
@@ -19,12 +19,8 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			get_template_part( 'template-parts/content', 'page' );
 
 		endwhile; // End of the loop.
 		?>
@@ -32,5 +28,4 @@ get_header();
     </main><!-- #main -->
 
 <?php
-//get_sidebar();
 get_footer();
