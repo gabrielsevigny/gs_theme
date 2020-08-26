9020775cfdde8e3d8e79c52501b218aecf522424
gabrielsevigny <gabrielsevigny@me.com>
2020-08-26 01:17:27 -0400

    add import slick and greensock

--- header.php
+++ header.php
@@ -25,31 +25,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'gs_theme' ); ?></a>
 
-    <header id="masthead" class="site-header">
-        <div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php
-			else :
-				?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-			$gs_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $gs_theme_description || is_customize_preview() ) :
-				?>
-                <p class="site-description"><?php echo $gs_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					?></p>
-			<?php endif; ?>
-        </div><!-- .site-branding -->
+    <header class="c-navbar">
+        <div class="c-navbar__brand"></div>
 
-        <nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gs_theme' ); ?></button>
+        <div class="c-navbar__Nav">
 			<?php
 			wp_nav_menu( array(
 					'theme_location' => 'menu-1',
@@ -57,5 +37,13 @@
 				)
 			);
 			?>
-        </nav><!-- #site-navigation -->
-    </header><!-- #masthead -->
+        </div>
+
+        <button class="c-burgerMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+        <div class="c-navbar__bg"></div>
+    </header>
